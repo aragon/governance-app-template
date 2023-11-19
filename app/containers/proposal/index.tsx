7e6a06fe3b93607c81980a82b76987409c07b0f2
Carlos Juarez <carlosgj94@gmail.com>
2023-11-19 18:47:55 +0000

    💄style💄: dynamic style for the proposal state

--- app/containers/proposal/index.tsx
+++ app/containers/proposal/index.tsx
@@ -89,25 +89,24 @@ export default function Proposal(props: ProposalInputs) {
 
   if (proposalLogs) return (
     <section className="pb-6 pt-10 dark:bg-dark lg:pb-[15px] lg:pt-[20px] w-5/6">
-      <div className="container mx-auto">
-        <div className="bg-blue-50 flex flex-wrap items-center justify-between rounded-lg border border-stroke bg-gray-2 px-6 py-8 dark:border-dark-3 dark:bg-dark-2 xs:px-10 md:px-8 lg:px-10">
-          <div className="w-full md:w-7/12 lg:w-2/3">
-            <div className="mb-6 md:mb-0">
-              <h4 className="mb-1 text-l font-semibold text-dark dark:text-white xs:text-xl md:text-l lg:text-xl">
-                {props.proposalId.toLocaleString()} - {ipfsResponse && ipfsResponse.title}
-              </h4>
-              <p className="text-base text-body-color dark:text-dark-6">
-                {ipfsResponse && ipfsResponse.summary}
-              </p>
-            </div>
-          </div>
+      <div className="bg-blue-50 flex justify-between rounded-lg border border-stroke px-4 py-5 dark:border-dark-3 dark:bg-dark-2 xs:px-10 md:px-6 lg:px-7 cursor-pointer">
+        <div className="">
+          <h4 className="mb-1 text-l font-semibold text-dark dark:text-white xs:text-xl md:text-l lg:text-xl">
+            {props.proposalId.toLocaleString()} - {ipfsResponse && ipfsResponse.title}
+          </h4>
+          <p className="text-base text-body-color dark:text-dark-6">
+            {ipfsResponse && ipfsResponse.summary}
+          </p>
+        </div>
 
-          <div className="w-full md:w-5/12 lg:w-1/3">
-            <div className="flex items-center space-x-3 md:justify-end">
-              <button className="inline-flex items-center justify-center rounded-md bg-white px-7 py-3 text-center text-base font-medium text-body-color shadow-1 dark:bg-dark dark:text-dark-6 dark:shadow-none">
-                {proposal?.open ? 'Open' : proposal?.executed ? 'Executed' : proposal?.tally.no >= proposal?.tally.yes ? 'Defeated' : 'To Execute'}
-              </button>
-            </div>
+        <div className="w-full md:w-5/12 lg:w-1/3">
+          <div className="flex items-center space-x-3 md:justify-end">
+            <p className={`
+              inline-flex items-center justify-center rounded-lg px-4 py-2 text-center text-base font-medium text-body-color shadow-1 dark:bg-dark dark:text-dark-6 dark:shadow-none border border-stroke
+              ${proposal?.open ? 'bg-gray-50 border-gray-600 color-gray-800' : proposal?.executed ? 'bg-green-400 border-green-700' : proposal?.tally.no >= proposal?.tally.yes ? 'bg-red-400 border-red-700' : 'bg-blue-400 border-blue-700'}
+            `}>
+              {proposal?.open ? 'Open' : proposal?.executed ? 'Executed' : proposal?.tally.no >= proposal?.tally.yes ? 'Defeated' : 'To Execute'}
+            </p>
           </div>
         </div>
       </div>

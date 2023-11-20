d61f4c6d6e1756e2a78335fd1515e107ec2ae417
Carlos Juarez <carlosgj94@gmail.com>
2023-11-20 15:01:27 +0000

    🔥feat🔥: add Aragon ODS

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { Web3Modal } from "./context/Web3Modal";
 import Header from "./containers/header";
 
+import '@aragon/ods/index.css';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,14 @@
 import type { Config } from 'tailwindcss'
 
-
 const config: Config = {
+  presets: [
+    require('@aragon/ods/tailwind.config'),
+  ],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@aragon/ods/**/*.js',
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: 'rgba(var(--color-primary-50), var(--tw-text-opacity))',
-          100: 'rgba(var(--color-primary-100), var(--tw-text-opacity))',
-          200: 'rgba(var(--color-primary-200), var(--tw-text-opacity))',
-          300: 'rgba(var(--color-primary-300), var(--tw-text-opacity))',
-          400: 'rgba(var(--color-primary-400), var(--tw-text-opacity))',
-          500: 'rgba(var(--color-primary-500), var(--tw-text-opacity))',
-          600: 'rgba(var(--color-primary-600), var(--tw-text-opacity))',
-          700: 'rgba(var(--color-primary-700), var(--tw-text-opacity))',
-          800: 'rgba(var(--color-primary-800), var(--tw-text-opacity))',
-          900: 'rgba(var(--color-primary-900), var(--tw-text-opacity))'
-        }
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
 }
 export default config

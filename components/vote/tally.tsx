4209a80bbedc9b31d548554e87c9d8397f0ec00a
Jør∂¡ <itsbrickpop@gmail.com>
2024-01-16 18:12:30 +0100

    Minor cleanup

--- components/vote/tally.tsx
+++ components/vote/tally.tsx
@@ -1,8 +1,6 @@
 import { VoteCastEvent } from "@/utils/types";
 import { FC, ReactNode } from "react";
 import { formatUnits } from "viem";
-// import Blockies from 'react-blockies';
-// import { If } from "@/components/if";
 
 interface VoteTallyProps {
   voteType: string;
@@ -19,23 +17,9 @@ const VoteTally: FC<VoteTallyProps> = ({ voteType, voteCount, votePercentage, vo
       <p className={`flex-grow text-xl text-${color}-700 font-semibold`}>{voteType}</p>
       <p className="text-xl font-semibold">{formatUnits(voteCount || BigInt(0), 18)}</p>
     </div>
-    {/** bg-success-700 bg-neutral-700 bg-critical-700 bg-success-100 bg-neutral-100 bg-critical-100*/}
     <div className={`h-4 w-full bg-${color}-100 rounded`}>
       <div className={`h-4 bg-${color}-700 rounded`} style={{ width: `${votePercentage}%` }}></div>
     </div>
-    {/* <div className="mt-4 grid grid-cols-5 space-between">
-      <If condition={votes?.length}>
-        {votes.filter(vote => vote.voteOption === option).map(vote => (
-          <div key={vote?.voter}>
-            <Blockies
-              size={7}
-              className="rounded-3xl"
-              seed={vote?.voter}
-              />
-          </div>
-        ))}
-      </If>
-    </div> */}
   </Card>
 );
 

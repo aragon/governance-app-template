932a25c903efdc3bb4ebde8d5253dceb3c8a457d
Carlos Juarez <carlosgj94@gmail.com>
2024-02-09 10:55:02 +0100

    refactor: improved view for the proposal cards

--- tokenVoting/components/proposal/index.tsx
+++ tokenVoting/components/proposal/index.tsx
@@ -3,8 +3,8 @@ import { usePublicClient } from "wagmi";
 import { Address } from "viem";
 import { Proposal } from "@/tokenVoting/utils/types";
 import { useProposal } from "@/tokenVoting/hooks/useProposal";
-import { Button } from "@aragon/ods";
-import { ReactNode } from "react";
+import { Card, Tag } from "@aragon/ods";
+import * as DOMPurify from 'dompurify';
 import { ButtonVariant } from "@aragon/ods/dist/types/src/components/button/button.api";
 import { PleaseWaitSpinner } from "@/components/please-wait";
 import { If } from "@/components/if";
@@ -22,19 +22,19 @@ type ProposalInputs = {
 const getProposalVariantStatus = (proposal: Proposal) => {
   return {
     variant: (proposal?.active
-      ? "secondary"
+      ? "primary"
       : proposal?.executed
-      ? "success"
-      : proposal?.tally?.no >= proposal?.tally?.yes
-      ? "critical"
-      : "success") as ButtonVariant,
+        ? "success"
+        : proposal?.tally?.no >= proposal?.tally?.yes
+          ? "critical"
+          : "success") as ButtonVariant,
     label: proposal?.active
       ? "Active"
       : proposal?.executed
-      ? "Executed"
-      : proposal?.tally?.no >= proposal?.tally?.yes
-      ? "Defeated"
-      : "Executable",
+        ? "Executed"
+        : proposal?.tally?.no >= proposal?.tally?.yes
+          ? "Defeated"
+          : "Executable",
   };
 };
 
@@ -50,8 +50,8 @@ export default function ProposalCard(props: ProposalInputs) {
 
   if (!proposal || showLoading) {
     return (
-      <section className="pb-3 pt-3 w-full">
-        <Card>
+      <section className="w-full mb-4">
+        <Card className="p-4">
           <span className="px-4 py-5 xs:px-10 md:px-6 lg:px-7">
             <PleaseWaitSpinner fullMessage="Loading proposal..." />
           </span>
@@ -60,73 +60,58 @@ export default function ProposalCard(props: ProposalInputs) {
     );
   } else if (status.metadataReady && !proposal?.title) {
     return (
-      <section className="pb-3 pt-3 w-full">
-        <Card>
-          <Link
-            href={`/proposals/${props.proposalId}`}
-            className="flex justify-between px-4 py-5 xs:px-10 md:px-6 lg:px-7 cursor-pointer"
-          >
-            <div className="md:w-7/12 lg:w-3/4 xl:4/5 pr-4 text-nowrap text-ellipsis overflow-hidden">
-              <h4 className="mb-1 text-lg text-neutral-300 line-clamp-1">
-                {Number(props.proposalId) + 1} -{" "}
-                {DEFAULT_PROPOSAL_METADATA_TITLE}
-              </h4>
-              <p className="text-base text-neutral-300 line-clamp-3">
-                {DEFAULT_PROPOSAL_METADATA_SUMMARY}
-              </p>
-            </div>
-          </Link>
+      <Link
+        href={`/proposals/${props.proposalId}`}
+        className="w-full mb-4"
+      >
+        <Card className="p-4">
+          <div className="md:w-7/12 lg:w-3/4 xl:4/5 pr-4 text-nowrap text-ellipsis overflow-hidden">
+            <h4 className="mb-1 text-lg text-neutral-300 line-clamp-1">
+              {Number(props.proposalId) + 1} -{" "}
+              {DEFAULT_PROPOSAL_METADATA_TITLE}
+            </h4>
+            <p className="text-base text-neutral-300 line-clamp-3">
+              {DEFAULT_PROPOSAL_METADATA_SUMMARY}
+            </p>
+          </div>
         </Card>
-      </section>
+      </Link>
     );
   }
 
   return (
-    <section className="pb-3 pt-3 w-full">
-      <Card>
-        <Link
-          href={`/proposals/${props.proposalId}`}
-          className="flex justify-between px-4 py-5 xs:px-10 md:px-6 lg:px-7 cursor-pointer"
-        >
-          <div className="md:w-7/12 lg:w-3/4 xl:4/5 pr-4 text-nowrap text-ellipsis overflow-hidden">
-            <h4 className="mb-1 text-lg font-semibold text-dark line-clamp-1">
-              {Number(props.proposalId) + 1} - {proposal.title}
-            </h4>
-            <p className="text-base text-body-color line-clamp-3">
-              {proposal.summary || DEFAULT_PROPOSAL_METADATA_SUMMARY}
-            </p>
+    <Link
+      href={`/proposals/${props.proposalId}`}
+      className="w-full cursor-pointer mb-4"
+    >
+      <Card className="p-4">
+        <If condition={proposal.tally}>
+          <div className="flex mb-2">
+            <Tag
+              variant={getProposalVariantStatus(proposal as Proposal).variant}
+              label={getProposalVariantStatus(proposal as Proposal).label}
+            />
           </div>
+        </If>
 
-          <div className="md:w-5/12 lg:w-1/4 xl:1/5">
-            <If condition={proposal.tally}>
-              <Button
-                className="w-full"
-                size="sm"
-                variant={getProposalVariantStatus(proposal as Proposal).variant}
-              >
-                {getProposalVariantStatus(proposal as Proposal).label}
-              </Button>
-            </If>
-          </div>
-        </Link>
+        <div className="text-ellipsis overflow-hidden">
+          <h4 className=" mb-1 text-lg font-semibold text-dark line-clamp-1">
+            {Number(props.proposalId) + 1} - {proposal.title}
+          </h4>
+          <p className="text-ellipsis overflow-hidden">
+            {<div className="text-ellipsis overflow-hidden box line-clamp-2"
+              dangerouslySetInnerHTML={{
+                __html: proposal.summary
+                  ? DOMPurify.sanitize(proposal.summary)
+                  : DEFAULT_PROPOSAL_METADATA_SUMMARY
+              }} />
+            }
+          </p>
+        </div>
       </Card>
-    </section>
+    </Link>
   );
 }
-
-// This should be encapsulated as soon as ODS exports this widget
-const Card = function ({ children }: { children: ReactNode }) {
-  return (
-    <div
-      className="w-full flex flex-col space-y-6 drop-shadow-sm hover:drop-shadow
-    box-border focus:outline-none focus:ring focus:ring-primary
-    hover:border-neutral-100 active:border-200
-    bg-neutral-0 rounded-xl"
-    >
-      {children}
-    </div>
-  );
-};
 
 function getShowProposalLoading(
   proposal: ReturnType<typeof useProposal>["proposal"],

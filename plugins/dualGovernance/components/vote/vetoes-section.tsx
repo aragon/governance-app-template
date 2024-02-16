d3082f134e905a8e2ca7f9eb563ad30389906318
Carlos Juarez <carlosgj94@gmail.com>
2024-02-16 12:36:41 +0100

    feat: adding the formatter in different parts

--- plugins/dualGovernance/components/vote/vetoes-section.tsx
+++ plugins/dualGovernance/components/vote/vetoes-section.tsx
@@ -3,14 +3,15 @@ import { VetoCastEvent } from "@/plugins/dualGovernance/utils/types";
 import { formatUnits } from "viem";
 import { AddressText } from "@/components/text/address";
 import { Card, Tag } from '@aragon/ods'
+import { formatLargeNumber } from "@/utils/formatNumber";
 
 export default function VetoesSection({ vetoes }: { vetoes: Array<VetoCastEvent> }) {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 mt-4 mb-14 gap-4">
       <div>
         <div className="grid gap-2">
-          {vetoes.map((vote, i) => (
-            <VetoCard key={i} vote={vote} />
+          {vetoes.map((veto, i) => (
+            <VetoCard key={i} veto={veto} />
           ))}
         </div>
       </div>
@@ -25,9 +26,9 @@ const VetoCard = function({ veto }: { veto: VetoCastEvent; }) {
         <div className="flex flex-grow">
           <Blockies className="rounded-3xl" size={9} seed={veto?.voter} />
           <div className="px-2">
-            <AddressText>{veto.voter}</AddressText>
+            <AddressText>{veto?.voter}</AddressText>
             <p className="text-neutral-600 text-sm">
-              {formatUnits(veto.votingPower, 18)} votes
+              {formatLargeNumber(formatUnits(veto.votingPower, 18))} votes
             </p>
           </div>
         </div>

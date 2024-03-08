eb8f80b148d40eb70bafa3cdb02d7fb2b0bca347
Jør∂¡ <itsbrickpop@gmail.com>
2024-03-08 13:52:32 +0100

    Adding a metadata loading spinner + simpler logic

--- plugins/dualGovernance/components/proposal/index.tsx
+++ plugins/dualGovernance/components/proposal/index.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
-import { usePublicClient } from "wagmi";
 import { useProposal } from "@/plugins/dualGovernance/hooks/useProposal";
 import { Card, Tag, TagVariant } from "@aragon/ods";
-import * as DOMPurify from 'dompurify';
+import * as DOMPurify from "dompurify";
 import { PleaseWaitSpinner } from "@/components/please-wait";
 import { useProposalVariantStatus } from "../../hooks/useProposalVariantStatus";
-import { PUB_DUAL_GOVERNANCE_PLUGIN_ADDRESS } from "@/constants";
 
 const DEFAULT_PROPOSAL_METADATA_TITLE = "(No proposal title)";
 const DEFAULT_PROPOSAL_METADATA_SUMMARY =
@@ -16,12 +14,7 @@ type ProposalInputs = {
 };
 
 export default function ProposalCard(props: ProposalInputs) {
-  const publicClient = usePublicClient();
-  const { proposal, status } = useProposal(
-    publicClient!,
-    PUB_DUAL_GOVERNANCE_PLUGIN_ADDRESS,
-    props.proposalId.toString()
-  );
+  const { proposal, status } = useProposal(props.proposalId.toString());
   const proposalVariant = useProposalVariantStatus(proposal!);
 
   const showLoading = getShowProposalLoading(proposal, status);
@@ -36,17 +29,24 @@ export default function ProposalCard(props: ProposalInputs) {
         </Card>
       </section>
     );
+  } else if (!proposal?.title && !proposal?.summary) {
+    // We have the proposal but no metadata yet
+    return (
+      <Link href={`#/proposals/${props.proposalId}`} className="w-full mb-4">
+        <Card className="p-4">
+          <span className="px-4 py-5 xs:px-10 md:px-6 lg:px-7">
+            <PleaseWaitSpinner fullMessage="Loading metadata..." />
+          </span>
+        </Card>
+      </Link>
+    );
   } else if (status.metadataReady && !proposal?.title) {
     return (
-      <Link
-        href={`/proposals/${props.proposalId}`}
-        className="w-full mb-4"
-      >
+      <Link href={`#/proposals/${props.proposalId}`} className="w-full mb-4">
         <Card className="p-4">
           <div className="md:w-7/12 lg:w-3/4 xl:4/5 pr-4 text-nowrap text-ellipsis overflow-hidden">
             <h4 className="mb-1 text-lg text-neutral-300 line-clamp-1">
-              {Number(props.proposalId) + 1} -{" "}
-              {DEFAULT_PROPOSAL_METADATA_TITLE}
+              {Number(props.proposalId) + 1} - {DEFAULT_PROPOSAL_METADATA_TITLE}
             </h4>
             <p className="text-base text-neutral-300 line-clamp-3">
               {DEFAULT_PROPOSAL_METADATA_SUMMARY}
@@ -63,12 +63,12 @@ export default function ProposalCard(props: ProposalInputs) {
       className="w-full cursor-pointer mb-4"
     >
       <Card className="p-4">
-          <div className="flex mb-2">
-            <Tag
-              variant={proposalVariant.variant as TagVariant}
-              label={proposalVariant.label}
-            />
-          </div>
+        <div className="flex mb-2">
+          <Tag
+            variant={proposalVariant.variant as TagVariant}
+            label={proposalVariant.label}
+          />
+        </div>
         <div className="text-ellipsis overflow-hidden">
           <h4 className=" mb-1 text-lg font-semibold text-dark line-clamp-1">
             {Number(props.proposalId) + 1} - {proposal.title}

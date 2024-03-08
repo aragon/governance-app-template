48d8756e4a575f34f860d2dba5dff22e8915dc48
Carlos Juarez <carlosgj94@gmail.com>
2024-03-08 11:34:09 +0100

    feat: using IndexDB for quick storage of all data

--- context/index.tsx
+++ context/index.tsx
@@ -1,12 +1,27 @@
 import { AlertProvider } from "./AlertContext";
 import { ReactNode } from "react";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query' 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { config } from "@/context/Web3Modal";
 import { createWeb3Modal } from '@web3modal/wagmi/react'
-import { State, WagmiProvider } from 'wagmi'
+import { State, WagmiProvider, deserialize, serialize } from 'wagmi'
 import { PUB_WALLET_CONNECT_PROJECT_ID } from "@/constants";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1_000 * 60 * 60 * 24, // 24 hours 
+    },
+  },
+});
+
+const persister = createAsyncStoragePersister({
+  serialize,
+  storage: AsyncStorage,
+  deserialize,
+})
 
 // Create modal
 createWeb3Modal({
@@ -15,14 +30,17 @@ createWeb3Modal({
   enableAnalytics: false // Optional - defaults to your Cloud configuration
 })
 
-export function RootContextProvider({ children, initialState }: { children: ReactNode,   initialState?: State }) {
+export function RootContextProvider({ children, initialState }: { children: ReactNode, initialState?: State }) {
   return (
     <WagmiProvider config={config} initialState={initialState}>
-      <QueryClientProvider client={queryClient}>
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ persister }}
+      >
         <AlertProvider>
           {children}
         </AlertProvider>
-      </QueryClientProvider>
+      </PersistQueryClientProvider>
     </WagmiProvider>
   );
 }

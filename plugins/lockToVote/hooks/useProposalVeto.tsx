26515ae0fb9d579aae71196c73d1e9f8354b269f
xavikh <xavikh@gmail.com>
2024-03-14 11:14:09 +0100

    Add permit to veto

--- plugins/lockToVote/hooks/useProposalVeto.tsx
+++ plugins/lockToVote/hooks/useProposalVeto.tsx
@@ -3,13 +3,18 @@ import {
   usePublicClient,
   useWaitForTransactionReceipt,
   useWriteContract,
+  useReadContracts,
+  useSignTypedData,
+  useAccount,
 } from "wagmi";
+import { hexToSignature } from "viem";
 import { useProposal } from "./useProposal";
 import { useProposalVetoes } from "@/plugins/dualGovernance/hooks/useProposalVetoes";
 import { useUserCanVeto } from "@/plugins/dualGovernance/hooks/useUserCanVeto";
-import { OptimisticTokenVotingPluginAbi } from "@/plugins/dualGovernance/artifacts/OptimisticTokenVotingPlugin.sol";
+import { LockToVetoPluginAbi } from "@/plugins/lockToVote/artifacts/LockToVetoPlugin.sol";
+import { ERC20Abi } from "@/plugins/lockToVote/artifacts/ERC20withPermit.sol";
 import { useAlertContext, AlertContextProps } from "@/context/AlertContext";
-import { PUB_CHAIN, PUB_LOCK_TO_VOTE_PLUGIN_ADDRESS } from "@/constants";
+import { PUB_CHAIN, PUB_TOKEN_ADDRESS, PUB_LOCK_TO_VOTE_PLUGIN_ADDRESS } from "@/constants";
 
 export function useProposalVeto(proposalId: string) {
   const publicClient = usePublicClient({ chainId: PUB_CHAIN.id });
@@ -27,6 +32,38 @@ export function useProposalVeto(proposalId: string) {
   );
 
   const { addAlert } = useAlertContext() as AlertContextProps;
+  const account_address = useAccount().address!;
+
+  const erc20Contract = {
+    address: PUB_TOKEN_ADDRESS,
+    abi: ERC20Abi,
+  };
+  const { data: erc20data } = useReadContracts({
+    contracts: [{
+      ...erc20Contract,
+      functionName: "balanceOf",
+      args: [account_address],
+    },{
+      ...erc20Contract,
+      functionName: "nonces",
+      args: [account_address],
+    },{
+      ...erc20Contract,
+      functionName: "name",
+    },{
+      ...erc20Contract,
+      functionName: "version",
+    }]
+  });
+
+  const [balanceResult, nonceResult, nameResult, versionResult] = erc20data || [];
+
+  const balance = BigInt(Number(balanceResult?.result));
+  const nonce = BigInt(Number(nonceResult?.result));
+  const erc20_name = nameResult?.result;
+  const versionFromContract = versionResult?.result;
+
+  const { signTypedData: vetoPermitSign } = useSignTypedData();
   const {
     writeContract: vetoWrite,
     data: vetoTxHash,
@@ -47,7 +84,8 @@ export function useProposalVeto(proposalId: string) {
           timeout: 4 * 1000,
         });
       } else {
-        addAlert("Could not create the proposal", { type: "error" });
+        console.error(vetoingError);
+        addAlert("Could not veto", { type: "error" });
       }
       return;
     }
@@ -72,12 +110,52 @@ export function useProposalVeto(proposalId: string) {
   }, [vetoingStatus, vetoTxHash, isConfirming, isConfirmed]);
 
   const vetoProposal = () => {
-    vetoWrite({
-      abi: OptimisticTokenVotingPluginAbi,
-      address: PUB_LOCK_TO_VOTE_PLUGIN_ADDRESS,
-      functionName: "veto",
-      args: [proposalId],
-    });
+    let deadline = BigInt(Math.floor(Date.now() / 1000) + 60 * 60);
+    let value = balance; 
+    let destination: `0x${string}` = PUB_LOCK_TO_VOTE_PLUGIN_ADDRESS;
+
+    const domain = {
+      chainId: PUB_CHAIN.id,
+      name: String(erc20_name),
+      /* We assume 1 if permit version is not specified */
+      version: String(versionFromContract ?? '1'),
+      verifyingContract: PUB_TOKEN_ADDRESS,
+    };
+
+    const types = {
+      Permit: [
+        { name: 'owner', type: 'address' },
+        { name: 'spender', type: 'address' },
+        { name: 'value', type: 'uint256' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'deadline', type: 'uint256' },
+      ],
+    };
+
+    const message = {
+      owner: account_address,
+      spender: destination,
+      value,
+      nonce,
+      deadline,
+    };
+
+    vetoPermitSign({
+      account: account_address,
+      types,
+      domain,
+      primaryType: 'Permit',
+      message,
+    }, { onSuccess: (hexSignature) => {
+      let signature = hexToSignature(hexSignature);
+
+      vetoWrite({
+        abi: LockToVetoPluginAbi,
+        address: destination,
+        functionName: "vetoPermit",
+        args: [proposalId, value, deadline, signature.v, signature.r, signature.s],
+      });
+    }});
   };
 
   return {

e658ca28989bceae29d8d8d3998cbcc7d8c703bf
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-09 07:58:58 -0400

    fix eslint issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   extends: ["next/core-web-vitals", "eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: ["@typescript-eslint"],
   rules: {
     "react/jsx-boolean-value": ["warn", "always"],

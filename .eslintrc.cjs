533fb207a24edba4eea76b78792e06cf665f1aef
Fabrice Francois <fabricevladimir@gmail.com>
2024-04-09 07:50:10 -0400

    merge eslint configs and update rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,16 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: { "@typescript-eslint/no-explicit-any": "off" },
+  extends: ["next/core-web-vitals", "eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "react/jsx-boolean-value": ["warn", "always"],
+    "no-console": "warn",
+    "prefer-template": "warn",
+    "@typescript-eslint/consistent-type-imports": ["warn", { fixStyle: "inline-type-imports" }],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { ignoreRestSiblings: true }],
+    "@typescript-eslint/prefer-nullish-coalescing": "warn",
+  },
   root: true,
 };

c283df51d0149c25daa54d76c8b4c17b7748cacc
Carlos Juarez <carlosgj94@gmail.com>
2024-04-17 20:23:39 +0200

    fix: this is not the core of the problem, but fixes it for now

--- context/Web3Modal.tsx
+++ context/Web3Modal.tsx
@@ -1,6 +1,6 @@
 import { http, createConfig } from "wagmi";
 import { injected } from "wagmi/connectors";
-import { walletConnect } from "wagmi/connectors";
+import { walletConnect, coinbaseWallet } from "wagmi/connectors";
 import {
   PUB_APP_DESCRIPTION,
   PUB_APP_NAME,
@@ -10,6 +10,7 @@ import {
   PUB_WALLET_ICON,
   PUB_WEB3_ENDPOINT,
 } from "@/constants";
+import { mainnet } from "viem/chains";
 
 // wagmi config
 const metadata = {
@@ -20,10 +21,11 @@ const metadata = {
 };
 
 export const config = createConfig({
-  chains: [PUB_CHAIN],
+  chains: [PUB_CHAIN, mainnet],
   ssr: true,
   transports: {
     [PUB_CHAIN.id]: http(PUB_WEB3_ENDPOINT, { batch: true }),
+    [mainnet.id]: http(PUB_WEB3_ENDPOINT, { batch: true }),
   },
   connectors: [
     walletConnect({
@@ -31,7 +33,6 @@ export const config = createConfig({
       metadata,
       showQrModal: false,
     }),
-    injected({ shimDisconnect: true }),
-    // coinbaseWallet({ appName: metadata.name, appLogoUrl: metadata.icons[0] }),
+    coinbaseWallet({ appName: metadata.name, appLogoUrl: metadata.icons[0] }),
   ],
 });
--- context/index.tsx
+++ context/index.tsx
@@ -28,6 +28,15 @@ createWeb3Modal({
   wagmiConfig: config,
   projectId: PUB_WALLET_CONNECT_PROJECT_ID,
   enableAnalytics: false, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional
+  themeMode: "light",
+  allWallets: "SHOW",
+  featuredWalletIds: [
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+    "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369",
+    "18388be9ac2d02726dbac9777c96efaac06d744b2f6d580fccdd4127a6d01fd1",
+    "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
+  ],
 });
 
 export function RootContextProvider({ children, initialState }: { children: ReactNode; initialState?: State }) {

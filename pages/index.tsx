ea83b7eb62c669fb584de50012d7ed9f1401abb1
Jør∂¡ <itsbrickpop@gmail.com>
2024-08-23 12:42:30 +0200

    Working with sepolia plugins

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,16 @@ import { type ReactNode } from "react";
 import { useAccount } from "wagmi";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
 import { Else, If, Then } from "@/components/if";
+import { PUB_APP_NAME } from "@/constants";
 
 export default function StandardHome() {
   const { isConnected } = useAccount();
   const { open } = useWeb3Modal();
 
   return (
-    <MainSection>
+    <MainSection narrow>
       <Card>
-        <h1 className="text-2xl font-[700] text-neutral-800">Welcome to Aragonette!</h1>
+        <h1 className="text-2xl font-[700] text-neutral-800">Welcome to {PUB_APP_NAME}!</h1>
         <p className="text-md text-neutral-400">
           A beaufitul DAO experience in a simple template that you can customize. Get started by connecting your wallet
           and selecting a plugin from the menu.

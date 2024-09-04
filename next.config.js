f06afd6f26a1c603ff31f5bc75d41af76374514c
Hamid Roohi <hamidroohi71@gmail.com>
2024-09-04 19:15:15 +0330

    fix(launch error): fix initial errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  transpilePackages: ["react-hook-mousetrap"],
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
